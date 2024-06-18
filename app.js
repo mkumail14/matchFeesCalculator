fd9b76bc257632da81133f9b097170fb248b9b7f
Mohammad Kumail Asghar <mkumail7860@gmail.com>
2024-06-19 00:56:56 +0500

    bug test2

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
-function submitRegisterForm(){
-    var totalPlayers=document.getElementById('totalPlayers').value
-    var groundPlayers=document.getElementById('groundSize').value
-    var estimatedGameTime=document.getElementById('estimatedGameTime').value
-    var gameFees=document.getElementById('gameFees').value
+function submitRegisterForm() {
+    // Get values from the input fields
+    var totalPlayers = document.getElementById('totalPlayers').value;
+    var groundPlayers = document.getElementById('groundSize').value;
+    var estimatedGameTime = document.getElementById('estimatedGameTime').value;
+    var gameFees = document.getElementById('gameFees').value;
 
-    if(totalPlayers=='' || groundPlayers=='' || estimatedGameTime=='' || gameFees=='' || estimatedGameTime<=0 || totalPlayers<=0 || groundPlayers<=0 || gameFees<=0){
-        document.getElementById('err').innerText='Please input all data correctly.';
-    }else if(groundPlayers > totalPlayers){
-        document.getElementById('err').innerText='Total players should be more then ground size.';
-    }else{
-        document.getElementById('err').innerText='';
-        localStorage.setItem('totalPlayers',totalPlayers);
-        localStorage.setItem('groundPlayers',groundPlayers);
-        localStorage.setItem('estimatedGameTime',estimatedGameTime);
-        localStorage.setItem('gameFees',gameFees);
-        document.getElementById('registerForm').style.display='none';
+    // Initialize error message element
+    var errorElement = document.getElementById('err');
+
+    // Validate input values
+    if (totalPlayers === '' || groundPlayers === '' || estimatedGameTime === '' || gameFees === '' || 
+        estimatedGameTime <= 0 || totalPlayers <= 0 || groundPlayers <= 0 || gameFees <= 0) {
+        errorElement.innerText = 'Please input all data correctly.';
+    } else if (parseInt(groundPlayers) > parseInt(totalPlayers)) {
+        errorElement.innerText = 'Total players should be more than ground size.';
+    } else {
+        // Clear error message
+        errorElement.innerText = '';
+
+        // Store values in localStorage
+        localStorage.setItem('totalPlayers', totalPlayers);
+        localStorage.setItem('groundPlayers', groundPlayers);
+        localStorage.setItem('estimatedGameTime', estimatedGameTime);
+        localStorage.setItem('gameFees', gameFees);
+
+        // Hide the registration form
+        document.getElementById('registerForm').style.display = 'none';
     }
 }
