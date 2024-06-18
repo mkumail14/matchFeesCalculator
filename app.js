f2a8eb7e943ff81aab27004a1eaafcd9106728a0
Mohammad Kumail Asghar <mkumail7860@gmail.com>
2024-06-19 00:43:12 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+function submitRegisterForm(){
+    var totalPlayers=document.getElementById('totalPlayers').value
+    var groundPlayers=document.getElementById('groundSize').value
+    var estimatedGameTime=document.getElementById('estimatedGameTime').value
+    var gameFees=document.getElementById('gameFees').value
+    
+    if(totalPlayers=='' || groundPlayers=='' || estimatedGameTime=='' || gameFees=='' || estimatedGameTime<=0 || totalPlayers<=0 || groundPlayers<=0 || gameFees<=0){
+        document.getElementById('err').innerText='Please input all data correctly.';
+    }else if(groundPlayers<totalPlayers){
+        document.getElementById('err').innerText='Total players should be more then ground size.';
+    }else{
+        document.getElementById('err').innerText='';
+        localStorage.setItem('totalPlayers',totalPlayers);
+        localStorage.setItem('groundPlayers',groundPlayers);
+        localStorage.setItem('estimatedGameTime',estimatedGameTime);
+        localStorage.setItem('gameFees',gameFees);
+        document.getElementById('registerForm').style.display='none';
+    }
+}
